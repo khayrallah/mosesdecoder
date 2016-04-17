fab747c25c7c2ce9e2219e9be0d344a9131e8808
Huda Khayrallah <hudak@berkeley.edu>
2016-04-17 18:09:30 -0400

    Update chipmunk-postprocessing.py

--- scripts/training/wrappers/chipmunk-postprocessing.py
+++ scripts/training/wrappers/chipmunk-postprocessing.py
@@ -2,24 +2,27 @@ import sys
 
 i = sys.argv[1] 
 o =  sys.argv[2] 
-first =True
+
 
 
 
 with open(o,"w") as fout:
   with open(i) as fin:
     for line in fin:
+      first =True
       line = line.split()
       if line[0] == "*END*":
           fout.write("\n")
           first = True
       else: 
-        if not first:
-          fout.write(" ")
-          first = False
         line = line[1:]
         words = []
         for word in line:
+          if first:
+            first = False
+          else:
+            fout.write(" ")
+            first = True
           words+= [word.split(":")[0]]
         fout.write(" ".join(words))
 
