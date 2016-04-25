00dd3b3aa7e70406d591b27ec1cb8cc820e9ab03
Huda Khayrallah <hudak@berkeley.edu>
2016-04-25 12:06:54 -0400

    Create chipmunk2stem+morph.py

---
+++ scripts/training/wrappers/chipmunk2stem+morph.py
@@ -0,0 +1,28 @@
+
+import sys
+
+i = sys.argv[1] 
+o =  sys.argv[2] 
+
+
+
+first =True
+with open(o,"w") as fout:
+  with open(i) as fin:
+    for line in fin:
+      line = line.split()
+      if line[0] == "*END*":
+          fout.write("\n")
+          first = True
+      else: 
+        line = line[1:]
+        words = []
+        for word in line:
+          if first:
+            first = False
+          else:
+            fout.write(" ")
+          words+= [word.split(":")[0]]
+        fout.write(" ".join(words))
+
+    
