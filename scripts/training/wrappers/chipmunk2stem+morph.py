83728a2e1b3ddda4dbb86ce53575fe940d7b9cc8
Huda Khayrallah <hudak@berkeley.edu>
2016-04-26 20:58:59 -0400

    Update chipmunk2stem+morph.py

--- scripts/training/wrappers/chipmunk2stem+morph.py
+++ scripts/training/wrappers/chipmunk2stem+morph.py
@@ -2,27 +2,32 @@
 import sys
 
 i = sys.argv[1] 
-o =  sys.argv[2] 
-
+o_stem =  sys.argv[2] 
+o_morph =  sys.argv[3] 
 
 
 first =True
-with open(o,"w") as fout:
-  with open(i) as fin:
-    for line in fin:
-      line = line.split()
-      if line[0] == "*END*":
-          fout.write("\n")
-          first = True
-      else: 
-        line = line[1:]
-        words = []
-        for word in line:
-          if first:
-            first = False
-          else:
-            fout.write(" ")
-          words+= [word.split(":")[0]]
-        fout.write(" ".join(words))
+with open(o_stem,"w") as fout_stem:
+  with open(o_morph,"w") as fout_morph:
+    with open(i) as fin:
+      for line in fin:
+        line = line.split()
+        if line[0] == "*END*":
+            fout.write("\n")
+            first = True
+        else: 
+          line = line[1:]
+          words_stem = []
+          words_morph = []
+          for word in line:
+            if first:
+              first = False
+            else:
+              word = word.split(":")
+              if "ROOT" in word[1]:
+                words_stem += word[0]
+              else: 
+                words_morph += word[0]
+          fout.write(" ".join(words))
 
     
