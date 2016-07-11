b8c4ef79aa8f6f49adaa2041a24eda7095200141
Huda Khayrallah <huda@login3.clsp.jhu.edu>
2016-07-11 10:18:32 -0400

    .

--- scripts/training/wrappers/chipmunk2stem+morph.py
+++ scripts/training/wrappers/chipmunk2stem+morph.py
@@ -13,21 +13,35 @@ with open(o_stem,"w") as fout_stem:
       for line in fin:
         line = line.split()
         if line[0] == "*END*":
-            fout.write("\n")
+            fout_stem.write("\n")
+            fout_morph.write("\n")
             first = True
+        elif line[0] ==":":
+          fout_stem.write(" :")
+          fout_morph.write(" :")
         else: 
           line = line[1:]
-          words_stem = []
-          words_morph = []
+          if first:
+            words_stem = []
+            words_morph = []
+            first = False
+          else:
+            words_stem = [" "]
+            words_morph = [" "]
           for word in line:
-            if first:
-              first = False
-            else:
-              word = word.split(":")
-              if "ROOT" in word[1]:
-                words_stem += word[0]
-              else: 
-                words_morph += word[0]
-          fout.write(" ".join(words))
+            #if first:
+            #  first = False
+            #else:
+            word = word.split(":")
+            if "ROOT" in word[1] or "SPECIAL" in word[1]:
+              words_stem += word[0]
+            else: 
+              words_morph += word[0]
+          if words_stem == [" "] or words_stem == []:
+            words_stem = words_morph
+          if words_morph == [" "] or  words_morph == []:
+            words_morph = words_stem
+          fout_stem.write("".join(words_stem))
+          fout_morph.write("".join(words_morph))
 
     
